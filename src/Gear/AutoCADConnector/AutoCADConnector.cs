4474e2c7aaf56cae6f178a2089d050f708f8babc
AleksandrBerezin <aleksandr.berezin402@gmail.com>
2021-12-27 08:33:28 +0700

    feature(AutoCADConnector): добавлен метод для проведения нагрузчного тестирования

--- src/Gear/AutoCADConnector/AutoCADConnector.cs
+++ src/Gear/AutoCADConnector/AutoCADConnector.cs
@@ -1,6 +1,11 @@
-﻿using Autodesk.AutoCAD.ApplicationServices;
+﻿using System.Diagnostics;
+using System.IO;
+using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.Runtime;
+using Builder;
+using Core;
 using Gear;
+using Microsoft.VisualBasic.Devices;
 
 namespace AutoCADConnector
 {
@@ -13,6 +18,29 @@ namespace AutoCADConnector
         public static void StartGearPlugin()
         {
             Application.ShowModelessWindow(new MainWindow());
+        }
+
+        [CommandMethod("StressTesting", CommandFlags.Session)]
+        public static void StressTesting()
+        {
+            var builder = new GearBuilder();
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var gearParameters = new GearParameters();
+            var streamWriter = new StreamWriter($"log.txt", true);
+            var currentProcess = Process.GetCurrentProcess();
+            var count = 0;
+
+            while (true)
+            {
+                builder.BuildGear(gearParameters);
+                var computerInfo = new ComputerInfo();
+                var usedMemory = (computerInfo.TotalPhysicalMemory - computerInfo.AvailablePhysicalMemory) *
+                                 0.000000000931322574615478515625;
+                streamWriter.WriteLine(
+                    $"{++count}\t{stopWatch.Elapsed:hh\\:mm\\:ss}\t{usedMemory}");
+                streamWriter.Flush();
+            }
         }
 
         /// <inheritdoc/>
